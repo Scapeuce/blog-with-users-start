266e06feeaa04e638dcf0ba26b18b0c788dcda1a
Andres <andres.lillo.p@gmail.com>
2021-02-06 00:55:46 +1300

    add PostgreSQL instead of SQLite

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from forms import CreatePostForm, CommentForm
 from forms import RegisterForm, LoginForm
 from functools import wraps
 from flask_gravatar import Gravatar
+import os
 from pprint import pprint
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro',
@@ -21,7 +22,7 @@ gravatar = Gravatar(app, size=100, rating='g', default='retro',
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
